6266f8a15f57be69f2eb4c3e7a693615c1116f53
Sajees XPS <sajees.jeya@gmail.com>
2021-12-24 14:35:04 -0500

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,8 +44,75 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer", 
+  helper: "clone", 
+  activate: function(event){
+    console.log("activate", this); 
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this); 
+  }, 
+  over: function(event) {
+    console.log("over", event.target); 
+  }, 
+  out: function(event) {
+    console.log("out", event.target); 
+  }, 
+  update: function(event) {
+    // array to store the task data in 
+    var tempArr = []; 
 
+    // loop over current set of children in sortable list 
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim(); 
 
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+    // add task data to the temp array as an abject 
+    tempArr.push({
+        text: text,
+        date: date
+    });
+});
+
+    // trim down list's ID to match object property 
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", ""); 
+
+    // update array on task object and save 
+    tasks[arrName] = tempArr; 
+    saveTasks(); 
+  },
+  stop: function(event) {
+    $(this).removeClass("dropover");
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-ghroup-item", 
+  tolerance: "touch", 
+  drop: function(event, ui) {
+    // remove dragged element from the dom
+    ui.draggable.remove(); 
+  },
+  over: function(event, ui) {
+    console.log("over"); 
+  }, 
+  out: function(event, ui) {
+    console.log("out"); 
+  }
+}); 
+ 
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
